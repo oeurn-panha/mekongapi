801159fa7d57e6a7905af2f2d03a7db51be4da93
oeurn-panha <96670448+oeurn-panha@users.noreply.github.com>
2023-11-23 17:53:02 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # URL_DATABASE = "postgresql://username:password@localhost:5432/db_name"
-URL_DATABASE = "postgresql://postgres:panha123@localhost:5432/mekongproject"
+URL_DATABASE = "postgresql://postgres:panha123@localhost:5432/postgres"
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
